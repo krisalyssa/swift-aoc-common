78d7858f8a448fc5a16cc7d0b96e8dbfd6d3d68c
Craig S. Cottingham <craig@cottingham.net>
2023-11-18 20:50:12 -0600

    chore: add copyright headers

--- Sources/Common/Input.swift
+++ Sources/Common/Input.swift
@@ -1,3 +1,13 @@
+/******************************************************************************
+ **
+ ** Copyright (c) 2023 Craig S. Cottingham
+ ** Licensed under the MIT License.
+ **
+ ** See https://github.com/CraigCottingham/swift-aoc-common/blob/main/LICENSE
+ ** for license information.
+ **
+ **/
+
 import Foundation
 
 public class Input {
