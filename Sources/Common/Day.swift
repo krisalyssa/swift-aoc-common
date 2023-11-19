5abfe07fb12bf95886615ac7d03a1f0e6dff4dc8
Craig S. Cottingham <craig@cottingham.net>
2023-11-18 22:04:56 -0600

    feat: add Day protocol

---
+++ Sources/Common/Day.swift
@@ -0,0 +1,34 @@
+/******************************************************************************
+ **
+ ** Copyright (c) 2023 Craig S. Cottingham
+ ** Licensed under the MIT License.
+ **
+ ** See https://github.com/CraigCottingham/swift-aoc-common/blob/main/LICENSE
+ ** for license information.
+ **
+ **/
+
+import Foundation
+
+public protocol Day {
+  func run(_ day: Int) throws
+  func part1(_ input: Input) throws
+  func part2(_ input: Input) throws
+}
+
+extension Day {
+  public func run(day: Int) throws {
+    let input = Input(day: day, relativeTo: #file)
+
+    try part1(input)
+    try part2(input)
+  }
+
+  public func part1(_ input: Input) throws {
+    fatalError("function \(#function) needs to be implemented")
+  }
+
+  public func part2(_ input: Input) throws {
+    fatalError("function \(#function) needs to be implemented")
+  }
+}
